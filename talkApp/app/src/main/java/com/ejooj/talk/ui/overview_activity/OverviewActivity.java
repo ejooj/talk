a410b18eeb45629edcc80b83b45f57de2c6a4516
ejooj <enzojjorge@gmail.com>
2019-03-11 16:47:46 -0300

    WIP feature Overview

---
+++ talkApp/app/src/main/java/com/ejooj/talk/ui/overview_activity/OverviewActivity.java
@@ -0,0 +1,15 @@
+package com.ejooj.talk.ui.overview_activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ejooj.talk.R;
+
+public class OverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overview);
+    }
+}
